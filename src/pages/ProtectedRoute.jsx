dfb6630f5394f1f5436df8afc17d73763d821d26
BELLAMKONDAAMBICAKIRANMAYEE <ambica930@gmail.com>
2026-05-18 21:27:50 +0530

    3rd changes

--- src/pages/ProtectedRoute.jsx
+++ src/pages/ProtectedRoute.jsx
@@ -1,12 +1,22 @@
-import React from 'react'
-import { Navigate } from 'react-router-dom'
+import React, { useEffect, useState } from "react";
+import { Navigate } from "react-router-dom";
+import axios from "axios";
 
-function ProtectedRoute({children}) {
-    const user=JSON.parse(localStorage.getItem('user'))
-    if(!user){
-       return <Navigate to="/login"/>
-    }
-  return children
+function ProtectedRoute({ children }) {
+  const [isAuth, setIsAuth] = useState(null);
+
+  useEffect(() => {
+    axios
+      .get("https://ecomflask.duckdns.org/api/admin/me", {
+        withCredentials: true,
+      })
+      .then(() => setIsAuth(true))
+      .catch(() => setIsAuth(false));
+  }, []);
+
+  if (isAuth === null) return <h3>Loading...</h3>;
+
+  return isAuth ? children : <Navigate to="/login" />;
 }
 
-export default ProtectedRoute
+export default ProtectedRoute;
